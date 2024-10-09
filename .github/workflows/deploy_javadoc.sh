164b017942d3da64c27c44058a05f1a07b9b73ca
Nishant Mishra <38082502+nishantsir57@users.noreply.github.com>
2024-10-09 14:18:05 +0530

    Update deploy_javadoc.sh

--- .github/workflows/deploy_javadoc.sh
+++ .github/workflows/deploy_javadoc.sh
@@ -4,18 +4,18 @@
 mvn javadoc:javadoc
 
 # Step 2: Checkout or Create gh-pages branch
-git checkout gh-pages || git checkout --orphan gh-pages
+# git checkout gh-pages || git checkout --orphan gh-pages
 
 # Step 3: Remove old files
 rm -rf *
 
 # Step 4: Copy the new Javadocs to the gh-pages branch
-cp -r target/site/apidocs/* .
+cp -r docs/* .
 
 # Step 5: Commit and Push to gh-pages
 git add .
 git commit -m "Deploy updated Javadocs"
-git push origin gh-pages
+git push origin master
 
 echo "Javadocs deployed successfully!"
 chmod +x deploy_javadoc.sh
